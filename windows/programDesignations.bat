6f69611d3cfdffc6506969aad68f07a01ef8b283
Nr1cha <vrm.richards@gmail.com>
2020-03-06 15:45:10 -0700

    adding any unoticed changes to master to not miss anything

---
+++ windows/programDesignations.bat
@@ -0,0 +1,13 @@
+chrome.exe
+ONENOTE.EXE
+lync.exe
+C:\Users\"user"\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
+EXCEL.EXE
+MSPUB.EXE
+OUTLOOK.EXE
+POWERPNT.EXE
+WINWORD.EXE
+
+
+::TOOLS OFFICE
+SCANPST.EXE
